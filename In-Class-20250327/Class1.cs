5d8b31a5ec535930137766344b397c7389671238
Jake Fraser <fraserjk@mail.uc.edu>
2025-03-27 11:44:30 -0400

    Add project files.

---
+++ In-Class-20250327/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace In_Class_20250327
+{
+    internal class Say_Goodbye
+    {
+        /// <summary>
+        /// Write a friendly message to the console
+        /// </summary>
+        public void Goodbye()
+        {
+            Console.WriteLine("Goodbye from Group 15");
+        }
+    }
+}
---
+++ In-Class-20250327/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace In_Class_20250327
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Say_Goodbye myObject;
+            myObject = new Say_Goodbye();
+
+            myObject.Goodbye();
+        }
+    }
+}
